d21055b9e44cc9d1c54bbd3d0469a5e6ce2e7520
Shawn <MistPhizzle@gmail.com>
2012-09-09 10:09:07 -0400

    Testing
    EXP and Item Save aren't working, no idea why.

--- src/Listeners/PlayerListener.java
+++ src/Listeners/PlayerListener.java
@@ -13,26 +13,26 @@ public class PlayerListener implements Listener{
 	public HashMap<String, ItemStack[]> deathinventory = new HashMap<String, ItemStack[]>();
 	public HashMap<String, ItemStack[]> deatharmor = new HashMap<String, ItemStack[]>();
 		
-	@EventHandler()
-    public void onPlayerDeathEvent(PlayerDeathEvent e) {
-    	if (e.getEntity().hasPermission("eu.saveitems")) {
-    		deathinventory.put(e.getEntity().getName(), e.getEntity().getInventory().getContents());
-    		deatharmor.put(e.getEntity().getName(), e.getEntity().getInventory().getArmorContents());
-    		e.getDrops().clear();
-    	}
-    	
-    	if (e.getEntity().hasPermission("eu.saveexp")) {
-    		e.setKeepLevel(true);
-    		e.setDroppedExp(0);
-    	}
+	@EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        if (e.getEntity().hasPermission("eu.dropsave")) {
+            deathinventory.put(e.getEntity().getName(), e.getEntity().getInventory().getContents());
+            deatharmor.put(e.getEntity().getName(), e.getEntity().getInventory().getArmorContents());
+            e.getDrops().clear();
+        }
+        
+        if (e.getEntity().hasPermission("eu.saveexp")) {
+            e.setKeepLevel(true);
+            e.setDroppedExp(0);
+        }
     }
     
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent e) {
-    	if (!deathinventory.containsKey(e.getPlayer().getName())) return;
-    	e.getPlayer().getInventory().setContents(deathinventory.get(e.getPlayer().getName()));
-    	e.getPlayer().getInventory().setArmorContents(deatharmor.get(e.getPlayer().getName()));
-    	deathinventory.remove(e.getPlayer().getName());
-    	deatharmor.remove(e.getPlayer().getName());
-    	}
-	}
+        if (!deathinventory.containsKey(e.getPlayer().getName())) return;
+        e.getPlayer().getInventory().setContents(deathinventory.get(e.getPlayer().getName()));
+        e.getPlayer().getInventory().setArmorContents(deatharmor.get(e.getPlayer().getName()));
+        deathinventory.remove(e.getPlayer().getName());
+        deatharmor.remove(e.getPlayer().getName());
+    }
+}
--- src/com/etriacraft/EtriaUtils/EtriaUtils.java
+++ src/com/etriacraft/EtriaUtils/EtriaUtils.java
@@ -11,18 +11,9 @@ public class EtriaUtils extends JavaPlugin {
 
 	public static final Logger log = Logger.getLogger("Minecraft");
 	public static final String PREFIX = "[EtriaUtils] ";
-		
-	public static EtriaUtils instance;
-	
-    public void loadConfiguration() {
-    	if (!new File(getDataFolder() + File.separator + "config.yml").exists())
-    		saveDefaultConfig();
-    }
 	
 	@Override
 	public void onEnable() {
-		
-		instance = this;
 		
 		this.getServer().getPluginManager().registerEvents(new SignListener(), this);
 		this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
@@ -31,10 +22,5 @@ public class EtriaUtils extends JavaPlugin {
 
 	public void onDisable() {
 	}
-	
-	
-	public static EtriaUtils getInstance() {
-    	return instance;
-    }
 	
 }
